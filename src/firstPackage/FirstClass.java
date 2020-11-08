f9b6f8427c7d13129c62a81d9d4ad7f2ba471282
Ramazan Soytekin <soytekinramazan@gmail.com>
2020-11-08 17:09:05 +0100

    second commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("first commit from local master branch");
+		System.out.println("second commit from local master");
 	}
 }
